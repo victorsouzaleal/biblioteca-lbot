ee4dfa0cd2a05e01e1547e514e57fba5176d327e
victorsouzaleal <victorsouzaleal@gmail.com>
2025-03-05 09:35:54 -0300

    categoria de IA removida junto com a biblioteca do Hercai que não funciona mais.

--- src/index.ts
+++ src/index.ts
@@ -6,9 +6,8 @@ import('@ffmpeg-installer/ffmpeg').then((ffmpegInstaller)=>{
 import * as audioLibrary from './lib/audio.js'
 import * as downloadLibrary from './lib/download.js'
 import * as generalLibrary from './lib/general.js'
-import * as aiLibrary from './lib/ai.js'
 import * as imageLibrary from './lib/image.js'
 import * as videoLibrary from './lib/video.js'
 import * as stickerLibrary from './lib/sticker.js'
 
-export {audioLibrary, downloadLibrary, generalLibrary, aiLibrary, imageLibrary, videoLibrary, stickerLibrary}
+export {audioLibrary, downloadLibrary, generalLibrary, imageLibrary, videoLibrary, stickerLibrary}
